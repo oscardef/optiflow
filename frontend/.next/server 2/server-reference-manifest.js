90739330deae0b2a1e1ed2b98a4a30858e1edc5e
arnaudlvq <arnaudlvq@gmail.com>
2025-12-07 18:03:19 +0100

    Remove .next from git and add to .gitignore

--- frontend/.next/server 2/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kgxSvyfL9YHAONHc7dpKfN610OUq4fm3jrwASw/DCQU=\"\n}"
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9B0IfGbt/YEI/JYqECe9h/hhCxkmbhmgU8sEsM30olI=\"\n}"
